eed2463e1cb973c8412ef9b363b7128789a0fec2
Brian Patrick <batpat772@gmail.com>
2017-11-06 22:56:00 -0800

    server.js logic added, need to worko on burger_controller for routes next

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var express = require('express');
 var method_Override = require('method-override');
-var bodyParser = require('bodyparser');
+var bodyParser = require('body-parser');
 
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+//Statis server content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Set Handlebars
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main "}));
+app.set("view engine", "handlebars");
+//Import routes and give the server access to them
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
+app.listen(PORT);

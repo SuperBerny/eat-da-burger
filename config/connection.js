5632a9b5d2d3a6fd81aea55693c87424b6ed3436
Brian Patrick <batpat772@gmail.com>
2017-11-10 11:17:53 -0800

    changed connection login info

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 //Setup MYSQL connection
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-   port: 3306,
-   host: "localhost",
-   user: "root",
-   password: "Qpzmgh&8",
-   database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+   connection = mysql.createConnection({
+   host: 'x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+   user: 'okp9fjgtx0jde39j',
+   password: 'oqf8o0rf6ty0uvc6',
+   database: 'todoagain_db'
+   });
+};
+
 //Make connection
 connection.connect(function(err) {
    if (err) {

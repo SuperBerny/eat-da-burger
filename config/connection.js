1aaf4da166bc7c0962f8eca10837e0a27d209dee
Brian Patrick <batpat772@gmail.com>
2017-11-11 14:31:17 -0800

    trying to get connection issue to work for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,24 @@
 var mysql = require('mysql');
 var connection;
 
+
+
+// connection = mysql.createConnection({
+//    host: 'x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//    user: 'okp9fjgtx0jde39j',
+//    password: 'oqf8o0rf6ty0uvc6',
+//    database: 'pemz0ljtl4lx72tu'
+// });
+
 if (process.env.JAWSDB_URL) {
    connection = mysql.createConnection(process.env.JAWSBD_URL);
 } else {
    connection = mysql.createConnection({
-   port: 3306,
-   host: 'x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-   user: 'okp9fjgtx0jde39j',
-   password: 'oqf8o0rf6ty0uvc6',
-   database: 'pemz0ljtl4lx72tu'
+      host: "localhost",
+      user: "root",
+      password: "Qpzmgh&8",
+      database: "burgers_db"
+   });
    });
 };
 

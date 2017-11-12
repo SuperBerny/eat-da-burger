a015b1ab24f75a474959ce70aae75b34af4d7c9e
Brian Patrick <batpat772@gmail.com>
2017-11-12 09:23:09 -0800

    another attemp to fix heroku issue

--- config/connection.js
+++ config/connection.js
@@ -11,30 +11,53 @@ var test;
 //    database: 'pemz0ljtl4lx72tu'
 // });
 
-if (process.env.JAWSDB_URL) {
-   connection = mysql.createConnection(process.env.JAWSBD_URL);
-   test = "I connected to JAWSDB";
-} else {
-   connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "Qpzmgh&8",
-      database: "pemz0ljtl4lx72tu"
-   });
-   test = "I connected to local connection";
-};
+// if (process.env.JAWSDB_URL) {
+//    connection = mysql.createConnection(process.env.JAWSBD_URL);
+//    test = "I connected to JAWSDB";
+// } else {
+//    connection = mysql.createConnection({
+//       host: "localhost",
+//       user: "root",
+//       password: "Qpzmgh&8",
+//       database: "pemz0ljtl4lx72tu"
+//    });
+//    test = "I connected to local connection";
+// };
 
-//Make connection
-connection.connect(function(err) {
-   console.log(test);
-   if (err) {
-      console.error("error connection: " + err.stack);
-      return;
-   };
-   console.log("connect as id: " + connection.threadId);
-});
+// //Make connection
+// connection.connect(function(err) {
+//    console.log(test);
+//    if (err) {
+//       console.error("error connection: " + err.stack);
+//       return;
+//    };
+//    console.log("connect as id: " + connection.threadId);
+// });
 
 //Export connection for ORM to use
 module.exports = connection;
 
-//mysql://okp9fjgtx0jde39j:oqf8o0rf6ty0uvc6@x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pemz0ljtl4lx72tu
+//Danny's code
+
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    database: "burgers_db",
+    user: "root"
+  });
+}
+
+connection.connect(err => {
+  if (err) {
+    return console.log(err.stack);
+  } else {
+    console.log("connected as id " + connection.threadId);
+  }
+});
+
+module.exports = connection;
